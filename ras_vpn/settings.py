55ce88402b7e1d946cef216c258dc36e4cb75f84
Santiago Bassani <santiago.bassani96@gmail.com>
2020-08-30 23:58:01 -0300

    [CHANGE] rename env var

--- ras_vpn/settings.py
+++ ras_vpn/settings.py
@@ -6,8 +6,8 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 
-vpn_username = os.getenv("USERNAME")
-vpn_password = os.getenv("PASSWORD")
+vpn_username = os.getenv("VPN_USERNAME")
+vpn_password = os.getenv("VPN_PASSWORD")
 
 CREDENTIALS_FILE = "cred.up"
 
